79d945afefb19fea4217e4d3426c6a5b667c762e
achrefbenabdallah <achref.ben-abdallah@tek-up.tn>
2021-04-28 16:06:24 +0100

    add firestore system and fetch data from there

--- app/src/main/java/com/example/ds_app/MainActivity.java
+++ app/src/main/java/com/example/ds_app/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.ds_app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+    TextView fullName,email,phone;
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullName=findViewById(R.id.profileName);
+        email=findViewById(R.id.profileEmail);
+        phone=findViewById(R.id.profilePhone);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+        userId=fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference=fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                phone.setText(value.getString("Phone"));
+                fullName.setText(value.getString("Fullname"));
+                email.setText(value.getString("Email"));
+            }
+        })
+
     }
 
     public void logout(View view) {
--- app/src/main/java/com/example/ds_app/register.java
+++ app/src/main/java/com/example/ds_app/register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +15,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
     EditText mFullname,mEmail,mPassword,mPhone;
@@ -24,6 +32,8 @@ public class register extends AppCompatActivity {
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +46,10 @@ public class register extends AppCompatActivity {
         mPhone=findViewById(R.id.phone);
         mRegisterBtn=findViewById(R.id.registerbtn);
         mLoginBtn=findViewById(R.id.createtext);
+        progressBar=findViewById(R.id.progressBar);
 
         fAuth=FirebaseAuth.getInstance();
-        progressBar=findViewById(R.id.progressBar);
+        fStore=FirebaseFirestore.getInstance();
 
         if (fAuth.getCurrentUser()!=null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
@@ -50,6 +61,9 @@ public class register extends AppCompatActivity {
             public void onClick(View view) {
                 String email=mEmail.getText().toString().trim();
                 String password=mPassword.getText().toString().trim();
+                String fullname=mFullname.getText().toString().trim();
+                String phone=mPhone.getText().toString().trim();
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is empty !!");
@@ -74,6 +88,24 @@ public class register extends AppCompatActivity {
 
                         if (task.isSuccessful()){
                             Toast.makeText(register.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                            userId=fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fStore.collection("users").document(userId);
+                            Map<String,Object> user=new HashMap<>();
+                            user.put("FullName",fullname);
+                            user.put("Email",email);
+                            user.put("Phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG", "onSuccess: User profile is created with UID"+userId);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("TAG", "onFailure: "+e.toString() );
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else {
                             Toast.makeText(register.this, "Error !"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
